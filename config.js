8db57da879b9814806770668f88ce4c2569b5341
wingsweihua <wingsweihua@users.noreply.github.com>
2026-09-06 09:52:52 +0000

    deploy: 78056362d623dbaa4699e2444dae0c024a5e9848

---
+++ config.js
@@ -0,0 +1,4 @@
+window.PAPER_SHARING_CONFIG = {
+  SHEET_APPEND_URL: "https://script.google.com/macros/s/AKfycby2yHwjdCo98mP0juazJFdsf7J6MnZ5YtOI7nnLEMEx/dev",
+  MY_SHEET_LINK: "https://docs.google.com/spreadsheets/d/1-qp-EuCCrm8tlPljmS3oojpeK7UGtOTtQiiIDvwMnmY/edit?usp=sharing",
+};
